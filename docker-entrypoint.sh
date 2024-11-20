9133dca2f883022a121f7d3a2b78db1da8481989
Shing Chan <shing@agape-1.com>
2024-11-20 12:03:15 -0800

    refactor: Default `websocket.gzlaunch` variable and allow dynamic web port assignment bash passthrough

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,22 @@
 #!/bin/bash
 # Check if the correct number of arguments were passed
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <GZ_SIM_OPTIONS> <WEBSOCKET_GZLAUNCH_FILE>
-Example: $0 \"-s --headless-rendering\" \"websocket.gzlaunch\""
+    echo "Usage: $0 <GZ_SIM_OPTIONS> <WEBSOCKET_PORT>
+Example: $0 \"-s --headless-rendering\" \"9002\""
     exit 1
 fi
 
 # Assign passed arguments to variables
 GZ_SIM_OPTIONS="$1"
-WEBSOCKET_GZLAUNCH_FILE="$2"
+WEBSOCKET_PORT="$2"
 
 # Start the first process
 gz sim $GZ_SIM_OPTIONS &
 
-xmlstarlet edit -L --update "//port" --value $WEBSOCKET_PORT $WEBSOCKET_GZLAUNCH_FILE
+xmlstarlet edit -L --update "//port" --value $WEBSOCKET_PORT $WEBSOCKET_GZLAUNCH_FILE #Assumes $WEBSOCKET_GZLAUNCH_FILE is a defined env variable 
 
 # Start the second process
-gz-launch $WEBSOCKET_GZLAUNCH_FILE &
+gz launch $WEBSOCKET_GZLAUNCH_FILE &
 
 # Wait for any process to exit
 wait -n
