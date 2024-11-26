b6eca894ae496112c12bbc83d01c81daeed7f41b
Shing Chan <shing@agape-1.com>
2024-11-26 11:58:34 -0800

    refactor: Switch from `${WEBSOCKET_GZLAUNCH_FILE}` -> `${WEBSOCKET_GZLAUNCH_PATH}`

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@
 
 # Help function
 function show_help() {
-    echo "Usage: ./docker-entrypoint.sh [--websocket-gzlaunch-file=<value>] [--websocket-port=<value>]"
-    echo "Example: ./docker-entrypoint.sh --websocket-gzlaunch-file=\"/websocket.gzlaunch\" --websocket-port=8080"
+    echo "Usage: ./docker-entrypoint.sh [--websocket-gzlaunch-path=<value>] [--websocket-port=<value>]"
+    echo "Example: ./docker-entrypoint.sh --websocket-gzlaunch-path=\"/websocket.gzlaunch\" --websocket-port=8080"
     echo
     echo "Arguments:"
-    echo "  --websocket-gzlaunch-file   Gazebo simulation options (default: \"$WEBSOCKET_GZLAUNCH_FILE\")"
+    echo "  --websocket-gzlaunch-path   Gazebo simulation options (default: \"$WEBSOCKET_GZLAUNCH_PATH\")"
     echo "  --websocket-port   Port for the websocket (default: \"$WEBSOCKET_PORT\")"
 }
 
@@ -21,8 +21,8 @@ function show_error_and_exit() {
 # Parse arguments
 for arg in "$@"; do
     case $arg in
-        --websocket-gzlaunch-file=*)
-            WEBSOCKET_GZLAUNCH_FILE="${arg#*=}"
+        --websocket-gzlaunch-path=*)
+            WEBSOCKET_GZLAUNCH_PATH="${arg#*=}"
             ;;
         --websocket-port=*)
             WEBSOCKET_PORT="${arg#*=}"
@@ -38,10 +38,10 @@ for arg in "$@"; do
 done
 
 # Dynamically assign the websocket port
-xmlstarlet edit -L --update "//port" --value $WEBSOCKET_PORT $WEBSOCKET_GZLAUNCH_FILE #Assumes $WEBSOCKET_GZLAUNCH_FILE is a defined env variable 
+xmlstarlet edit -L --update "//port" --value $WEBSOCKET_PORT $WEBSOCKET_GZLAUNCH_PATH #Assumes $WEBSOCKET_GZLAUNCH_FILE is a defined env variable 
 
 # Start websocket server in background
-gz launch $WEBSOCKET_GZLAUNCH_FILE &
+gz launch $WEBSOCKET_GZLAUNCH_PATH &
 
 # Launch Gazebo Simulation
 gz sim ${GZ_SIM_OPTIONS}
